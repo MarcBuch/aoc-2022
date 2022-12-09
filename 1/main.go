c3ac3a875ef54f07763be44e9a7ccc70f73f2136
Marc Buchardt <marc@buchardtprivat.de>
2022-12-09 16:39:50 +0100

    feat(1): Add second part of day 1

--- 1/main.go
+++ 1/main.go
@@ -5,15 +5,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strconv"
 )
 
-func getHighestCalories(inputFile *os.File) int {
+func readFile(path string) []int {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	var inputSlice []int
 	var buffer int
 
 	// Parse the file contents
-	reader := bufio.NewReader(inputFile)
+	reader := bufio.NewReader(f)
 	for line, _, err := reader.ReadLine(); err != io.EOF; line, _, err = reader.ReadLine() {
 		// Check if line is not empty
 		if len(line) > 0 {
@@ -22,17 +28,20 @@ func getHighestCalories(inputFile *os.File) int {
 			// Convert string to int
 			lineInt, _ := strconv.Atoi(lineStr)
 			buffer += lineInt
-		// If line is empty (empty line has been declared as delimiter)
+			// If line is empty (empty line has been declared as delimiter)
 		} else {
 			// append the buffer int to the inputSlice and clear the buffer
 			inputSlice = append(inputSlice, buffer)
 			buffer = 0
 		}
 	}
-	
+	return inputSlice
+}
+
+func getHighestCalories(input []int) int {
 	// Get highest int
 	var highest, temp int
-	for _, element := range inputSlice {
+	for _, element := range input {
 		if element > temp {
 			temp = element
 			highest = temp
@@ -41,11 +50,20 @@ func getHighestCalories(inputFile *os.File) int {
 	return highest
 }
 
-func main() {
-	f, err := os.Open("./input.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
+func topThreeCaloriesInTotal(input []int) int {
+	// Sort the slice to descending
+	sort.Slice(input, func(i, j int) bool {
+		return input[i] > input[j]
+	})
 
-	fmt.Println(getHighestCalories(f))
+	return input[0] + input[1] + input[2]
+}
+
+func main() {
+
+	input := readFile("./input.txt")
+
+	fmt.Printf("The elf with the highest calories has: %d calories in his bag.\n", getHighestCalories(input))
+	fmt.Println("--------")
+	fmt.Printf("The top three elfs are carying %d calories in total.\n", topThreeCaloriesInTotal(input))
 }
